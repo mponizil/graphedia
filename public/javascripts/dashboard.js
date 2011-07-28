e60a428ae69dddf7e53fff1f331dd2b960f10b4f
Abe Stanway <abestanway@gmail.com>
2011-07-27 23:52:21 -0400

    live clicks, hover

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -25,3 +25,17 @@ socket.on('reply', function(data) {
   
   $('#column2 .jspPane').prepend(reply);
 })
+
+$(function() {
+  $('.commentScroller').jScrollPane({showArrows:true});
+});
+
+$(".firehose-comment").live("mouseover mouseout click", function(event) {
+  if ( event.type == "mouseover" ) {
+    $(this).css({'border': '3px solid red', 'border-radius':'5px', 'margin-top': '-3px', 'margin-bottom': '12px'})
+  } else if (event.type == "mouseout") {
+    $(this).css({'border': 'none', 'margin-top': '0px', 'margin-bottom': '15px'})
+  } else {
+    window.location = $(this).attr('js_value');
+  }
+});
